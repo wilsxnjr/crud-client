485f60e5927c578b7ac87800c335a704804b1010
wilsxnjr <wilsonmachavajunior@gmail.com>
2026-08-16 20:13:46 +0200

    first commit

---
+++ src/main/java/com/gestio/crudclient/CrudClientApplication.java
@@ -0,0 +1,13 @@
+package com.gestio.crudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gestio/crudclient/CrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestio.crudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
